879482401f49c11872b91827a1230919a9479b08
Edgar <mmnledgar@gmail.com>
2023-04-17 11:02:49 +0000

    adding data

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
     MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.red,
         appBar: AppBar(
-          title: Text('Dicee'),
+          centerTitle: true,
+          title: Text(
+            'Dicee',
+            textAlign: TextAlign.center,
+          ),
           backgroundColor: Colors.red,
         ),
         body: DicePage(),
@@ -15,9 +21,65 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  const DicePage({super.key});
+
+  @override
+  _DicePage createState() => _DicePage();
+}
+
+class _DicePage extends State<DicePage> {
+  int index_first = 1;
+  int index_second = 2;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      alignment: Alignment.center,
+      padding: EdgeInsets.all(20.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Expanded(
+            child: Padding(
+              padding: EdgeInsets.all(20.0),
+              child: ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    index_first = new Random().nextInt(6) + 1;
+                  });
+                },
+                style: ButtonStyle(
+                    elevation: MaterialStatePropertyAll<double>(0),
+                    backgroundColor:
+                        MaterialStatePropertyAll<Color>(Colors.red)),
+                child: Image.asset(
+                  "images/dice${index_first}.png",
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: EdgeInsets.all(20.0),
+              child: ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    index_second = new Random().nextInt(6) + 1;
+                  });
+                },
+                style: ButtonStyle(
+                    elevation: MaterialStatePropertyAll<double>(0),
+                    backgroundColor:
+                        MaterialStatePropertyAll<Color>(Colors.red)),
+                child: Image.asset(
+                  "images/dice${index_second}.png",
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
